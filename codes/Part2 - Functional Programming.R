998da69c903a0d28ecb537a3b8072b302e88fa7f
Harry Yang <harry.seunghoon.yang@gmail.com>
2018-04-23 22:32:26 +0900

    functional programming is too hard to understand.

--- codes/Part2 - Functional Programming.R
+++ codes/Part2 - Functional Programming.R
@@ -635,3 +635,544 @@ rollapply <- function(x, n, f, ...) {
 plot(x)
 lines(rollapply(x, 5, median), col = "red", lwd = 2)
 
+### 11.2.4 Parallelisation
+
+lapply3 <- function(x, f, ...) {
+  out <- vector("list", length(x))
+  for (i in sample(seq_along(x))) {
+    out[[i]] <- f(x[[i]], ...)
+  }
+  out
+}
+unlist(lapply(1:10, sqrt))
+unlist(lapply3(1:10, sqrt))
+
+library(parallel)
+unlist(mclapply(1:10, sqrt, mc.cores = 4))
+
+boot_df <- function(x) x[sample(nrow(x), replace = TRUE),]
+rsquared <- function(mod) summary(mod)$r.square
+boot_lm <- function(i) {
+  rsquared(lm(mpg ~ wt + disp, data = boot_df(mtcars)))
+}
+
+system.time(lapply(1:500, boot_lm))
+system.time(mclapply(1:500, boot_lm, mc.cores = 2))
+
+### 11.2.5 Exercises
+
+#1
+vapply(mtcars, sd, numeric(1))
+vapply(ggplot2::mpg[vapply(ggplot2::mpg, is.numeric, logical(1))], sd, numeric(1))
+
+#2
+sapply(ggplot2::mpg, class)
+
+### example ###
+a <- letters[1:3]
+class(a) <- c("class1", "class2")
+df <- data.frame(a = character(3))
+df$a <- a
+df$b <- a
+class(sapply(df, class))
+
+#3
+trials <- replicate(
+  100,
+  t.test(rpois(10, 10), rpois(7, 10)),
+  simplify = FALSE
+)
+sapply(trials, "[[", "p.value")
+
+#4
+#5
+lapply_nms <- function(X, FUN, ...){
+  Map(FUN, X, names(X), ...)
+}
+lapply_nms(iris, function(x, y) c(class(x), y))
+
+#6
+testlist <- list(iris, mtcars, cars)
+lapply(testlist, function(x) vapply(x, mean, numeric(1)))
+testlist
+
+#7
+
+## 11.3 Manipulating matrices and data frames
+
+## apply / tapply / plyr
+
+### 11.3.1 Matrix and array operations
+
+a <- matrix(1:20, nrow = 5)
+apply(a, 1, mean)
+apply(a, 2, mean)
+
+a1 <- apply(a, 1, identity)
+identical(a, a1)
+identical(a, t(a1))
+
+a2 <- apply(a, 2, identity)
+identical(a, a2)
+
+x <- matrix(rnorm(20, 0, 10), nrow = 4)
+x1 <- sweep(x, 1, apply(x, 1, min), `-`)
+x2 <- sweep(x1, 1, apply(x1, 1, max), `/`)
+x
+x1
+x2
+
+outer(1:3, 1:10, "*")
+
+### 11.3.2 Group apply
+
+pulse <- round(rnorm(22, 70, 10 / 3)) + rep(c(0, 5), c(10, 12))
+group <- rep(c("A", "B"), c(10, 12))
+
+tapply(pulse, group, length)
+tapply(pulse, group, mean)
+
+split(pulse, group)
+
+tapply2 <- function(x, group, f, ..., simplify = TRUE) {
+  pieces <- split(x, group)
+  sapply(pieces, f, simplify = simplify)
+}
+tapply2(pulse, group, length)
+tapply2(pulse, group, mean)
+
+### 11.3.3 The plyr package
+
+### 11.3.4 Exercises
+
+#1. 
+
+# for two dimensional cases everything is sorted by the other dimension
+arr2 <- array(1:9, dim = c(3, 3), dimnames = list(paste0("row", 1:3),
+                                                  paste0("col", 1:3)))
+arr2
+apply(arr2, 1, head, 1) # Margin is row
+apply(arr2, 1, head, 9) # sorts by col
+
+apply(arr2, 2, head, 1) # Margin is col
+apply(arr2, 2, head, 9) # sorts by row
+
+# 3 dimensional
+arr3 <- array(1:27, dim = c(3,3,3), dimnames = list(paste0("row", 1:3),
+                                                    paste0("col", 1:3),
+                                                    paste0("time", 1:3)))
+arr3
+apply(arr3, 1, head, 1) # Margin is row
+apply(arr3, 1, head, 27) # sorts by time and col
+
+apply(arr3, 2, head, 1) # Margin is col
+apply(arr3, 2, head, 27) # sorts by time and row
+
+apply(arr3, 3, head, 1) # Margin is time
+apply(arr3, 3, head, 27) # sorts by col and row
+
+# 4 dimensional
+arr4 <- array(1:81, dim = c(3,3,3,3), dimnames = list(paste0("row", 1:3),
+                                                      paste0("col", 1:3),
+                                                      paste0("time", 1:3),
+                                                      paste0("var", 1:3)))
+arr4
+
+apply(arr4, 1, head, 1) # Margin is row
+apply(arr4, 1, head, 81) # sorts by var, time, col
+
+apply(arr4, 2, head, 1) # Margin is col
+apply(arr4, 2, head, 81) # sorts by var, time, row
+
+apply(arr4, 3, head, 1) # Margin is time
+apply(arr4, 3, head, 81) # sorts by var, col, row
+
+apply(arr4, 4, head, 1) # Margin is var
+apply(arr4, 4, head, 81) # sorts by time, col, row
+
+#2
+v_tapply <- function(x, group, f, FUN.VALUE, ..., USE.NAMES = TRUE) {
+  pieces <- split(x, group)
+  vapply(pieces, f, FUN.VALUE, ..., USE.NAMES = TRUE)
+}
+
+#3
+split2 <- function(x, f, drop = FALSE, ...){
+  # there are three relevant cases for f. f is a character, f is a factor and all
+  # levels occur, f is a factor and some levels don't occur.
+  
+  # first we check if f is a factor
+  fact <- is.factor(f)
+  
+  # if drop it set to TRUE, we drop the non occuring levels.
+  # (If f is a character, this has no effect.)
+  if(drop){f <- f[, drop = TRUE]}
+  
+  # now we want all unique elements/levels of f
+  levs <- if (fact) {unique(levels(f))} else {as.character(unique(f))}
+  
+  # we use these levels to subset x and supply names for the resulting output.
+  setNames(lapply(levs, function(lv) x[f == lv, , drop = FALSE]), levs)
+}
+
+#4.
+
+##11.4 Manipulating lists
+
+###11.4.1 Reduce()
+Reduce(`+`, 1:3) # -> ((1 + 2) + 3)
+Reduce(sum, 1:3) # -> sum(sum(1, 2), 3)
+
+Reduce2 <- function(f, x) {
+  out <- x[[1]]
+  for(i in seq(2, length(x))) {
+    out <- f(out, x[[i]])
+  }
+  out
+}
+
+l <- replicate(5, sample(1:10, 15, replace = TRUE), simplify = FALSE)
+str(l)
+
+intersect(intersect(intersect(intersect(l[[1]], l[[2]]),
+                              l[[3]]), l[[4]]), l[[5]])
+Reduce(intersect, l)
+
+### 11.4.2 Predicate functionals
+
+where <- function(f, x) {
+  vapply(x, f, logical(1))
+}
+
+df <- data.frame(x = 1:3, y = c("a", "b", "c"))
+where(is.factor, df)
+str(Filter(is.factor, df))
+
+str(Find(is.factor, df))
+Position(is.factor, df)
+
+### 11.4.3 Exercises
+
+#2
+vapply_num <- function(x, fun, fun.value) {
+  vapply(Filter(is.numeric, x), fun, fun.value)
+}
+
+#3
+#4
+Any <- function(l, pred){
+  stopifnot(is.list(l))
+  
+  for (i in seq_along(l)){
+    if (pred(l[[i]])) return(TRUE)
+  }
+  
+  return(FALSE)
+}
+All <- function(l, pred){
+  stopifnot(is.list(l))
+  
+  for (i in seq_along(l)){
+    if (!pred(l[[i]])) return(FALSE)
+  }
+  
+  return(TRUE)
+}
+
+#5
+span_r <- function(l, pred){
+  # We test if l is a list
+  stopifnot(is.list(l))
+  
+  # we preallocate a logical vector and save the result
+  # of the predicate function applied to each element of the list
+  test <- vector("logical", length(l))
+  for (i in seq_along(l)){
+    test[i] <- (pred(l[[i]]))
+  }
+  # we return NA, if the output of pred is always FALSE
+  if(!any(test)) return(NA_integer_)
+  
+  # Otherwise we look at the length encoding of TRUE and FALSE values.
+  rle_test <- rle(test)
+  # Since it might happen, that more than one maximum series of TRUE's appears,
+  # we have to implement some logic, which might be easier, if we save the rle 
+  # output in a data.frmame
+  rle_test <- data.frame(lengths = rle_test[["lengths"]],
+                         values = rle_test[["values"]],
+                         cumsum = cumsum(rle_test[["lengths"]]))
+  rle_test[["first_index"]] <- rle_test[["cumsum"]] - rle_test[["lengths"]] + 1
+  # In the last line we calculated the first index in the original list for every encoding
+  # In the next line we calculate a column, which gives the maximum 
+  # encoding length among all encodings with the value TRUE
+  rle_test[["max"]] <-  max(rle_test[rle_test[, "values"] == TRUE, ][,"lengths"])
+  # Now we just have to subset for maximum length among all TRUE values and return the
+  # according "first index":
+  rle_test[rle_test$lengths == rle_test$max & rle_test$values == TRUE, ]$first_index
+}
+
+## 11.5 Mathematical functionals
+integrate(sin, 0, pi)
+str(uniroot(sin, pi * c(1 / 2, 3 / 2)))
+
+str(optimize(sin, c(0, 2 * pi)))
+str(optimize(sin, c(0, pi), maximum = TRUE))
+
+poisson_nll <- function(x) {
+  n <- length(x)
+  sum_x <- sum(x)
+  function(lambda) {
+    n * lambda - sum_x * log(lambda)
+  }
+}
+
+x1 <- c(41, 30, 31, 38, 29, 24, 30, 29, 31, 38)
+x2 <- c(6, 4, 7, 3, 3, 7, 5, 2, 2, 7, 5, 4, 12, 6, 9)
+nll1 <- poisson_nll(x1)
+nll2 <- poisson_nll(x2)
+
+optimise(nll1, c(0, 100))$minimum
+optimise(nll2, c(0, 100))$minimum
+
+### 11.5.1 Exercises
+
+arg_max <- function(x, f){
+  x[f(x) == max(f(x))]
+}
+arg_max(-10:5, function(x) x^2)
+arg_min <- function(x, f){
+  x[f(x) == min(f(x))]
+}
+arg_min(-5:5, function(x) x^2)
+
+## 11.6 Loops that should be left as is
+
+trans <- list(
+  disp = function(x) x * 0.0163871,
+  am = function(x) factor(x, levels = c("auto", "manual"))
+)
+for(var in names(trans)) {
+  mtcars[[var]] <- trans[[var]](mtcars[[var]])
+}
+mtcars
+
+lapply(names(trans), function(var) {
+  mtcars[[var]] <<- trans[[var]](mtcars[[var]])
+})
+
+### 11.6.2 Recusrive relationships
+
+exps <- function(x, alpha) {
+  s <- numeric(length(x) + 1)
+  for (i in seq_along(s)) {
+    if (i == 1) {
+      s[i] <- x[i]
+    } else {
+      s[i] <- alpha * x[i - 1] + (1 - alpha) * s[i - 1]
+    }
+  }
+  s
+}
+x <- runif(6)
+exps(x, 0.5)
+
+### 1.6.3 While loops
+
+for ( i in 1:10) print(i)
+
+i <- 1
+while(i <= 10) {
+  print(i)
+  i <- i + 1
+}
+
+# i <- rgeom(0, 1)
+i <- 0
+while(TRUE) {
+  if (runif(1) > 0.9) break
+  i <- i + 1
+}
+i
+
+## 11.7 A family of functions
+
+add <- function(x, y) {
+  stopifnot(length(x) == 1, length(y) == 1,
+            is.numeric(x), is.numeric(y))
+  x + y
+}
+
+rm_na <- function(x, y, identity) {
+  if (is.na(x) && is.na(y)) {
+    identity
+  } else if (is.na(x)) {
+    y
+  } else {
+    x
+  }
+}
+rm_na(NA, 10, 0)
+rm_na(10, NA, 0)
+rm_na(NA, NA, 0)
+
+add <- function(x, y, na.rm = FALSE) {
+  if (na.rm && (is.na(x) || is.na(y))) rm_na(x, y, 0) else x + y
+}
+add(10, NA)
+add(10, NA, na.rm = TRUE)
+add(NA, NA)
+add(NA, NA, na.rm = TRUE)
+
+add(add(3, NA, na.rm = TRUE), NA, na.rm = TRUE)
+add(3, add(NA, NA, na.rm = TRUE), na.rm = TRUE)
+
+r_add <- function(xs, na.rm = TRUE) {
+  Reduce(function(x, y) add(x, y, na.rm = na.rm), xs)
+}
+r_add(c(1, 4, 10))
+
+r_add(NA, na.rm = TRUE)
+r_add(numeric())
+
+r_add <- function(xs, na.rm = TRUE) {
+  Reduce(function(x, y) add(x, y, na.rm = na.rm), xs, init = 0)
+}
+r_add(c(1, 4, 10))
+r_add(NA, na.rm = TRUE)
+r_add(numeric())
+
+v_add1 <- function(x, y, na.rm = FALSE) {
+  stopifnot(length(x) == length(y), is.numeric(x), is.numeric(y))
+  if (length(x) == 0) return(numeric())
+  simplify2array(
+    Map(function(x, y) add(x, y, na.rm = na.rm), x, y)
+  )
+}
+
+v_add2 <- function(x, y, na.rm = FALSE) {
+  stopifnot(length(x) == length(y), is.numeric(x), is.numeric(y))
+  vapply(seq_along(x), function(i) add(x[i], y[i], na.rm = na.rm),
+         numeric(1))
+}
+
+v_add1(1:10, 1:10)
+v_add1(numeric(), numeric())
+
+v_add1(c(1, NA), c(1, NA))
+v_add1(c(1, NA), c(1, NA), na.rm = TRUE)
+
+c_add <- function(xs, na.rm = FALSE) {
+  Reduce(function(x, y) add(x, y, na.rm = na.rm), xs,
+         accumulate = TRUE)
+}
+c_add(1:10)
+c_add(10:1)
+
+row_sum <- function(x, na.rm = FALSE) {
+  apply(x, 1, add, na.rm = na.rm)
+}
+col_sum <- function(x, na.rm = FALSE) {
+  apply(x, 2, add, na.rm = na.rm)
+}
+arr_sum <- function(x, dim, na.rm = FALSE) {
+  apply(x, dim, add, na.rm = na.rm)
+}
+
+### 11.7.1 Exercises
+
+#1
+smaller_ <- function(x, y){
+  if(anyNA(c(x, y))){return(NA_integer_)}
+  out <- x
+  if(y < x) {out <- y}
+  out
+}
+
+larger_ <- function(x, y){
+  if(anyNA(c(x, y))){return(NA_integer_)}
+  out <- x
+  if(y > x) {out <- y}
+  out
+}
+
+rm_na <- function(x, y, identity) {
+  if (is.na(x) && is.na(y)) {
+    identity
+  } else if (is.na(x)) {
+    y
+  } else {
+    x
+  }
+}
+
+smaller <- function(x, y, na.rm = FALSE) {
+  stopifnot(length(x) == 1, length(y) == 1, is.numeric(x) | is.logical(x),
+            is.numeric(y) | is.logical(y))
+  if (na.rm && (is.na(x) || is.na(y))) rm_na(x, y, Inf) else smaller_(x,y)
+}
+
+larger <- function(x, y, na.rm = FALSE) {
+  stopifnot(length(x) == 1, length(y) == 1, is.numeric(x) | is.logical(x),
+            is.numeric(y) | is.logical(y))
+  if (na.rm && (is.na(x) || is.na(y))) rm_na(x, y, -Inf) else larger_(x,y)
+}
+
+r_smaller <- function(xs, na.rm = TRUE) {
+  Reduce(function(x, y) smaller(x, y, na.rm = na.rm), xs, init = Inf)
+}
+r_smaller(c(1:3, 4:(-1)))
+r_smaller(NA, na.rm = TRUE)
+r_smaller(numeric())
+
+r_larger <- function(xs, na.rm = TRUE) {
+  Reduce(function(x, y) larger(x, y, na.rm = na.rm), xs, init = -Inf)
+}
+r_larger(c(1:3), c(4:1))
+r_larger(NA, na.rm = TRUE)
+r_larger(numeric())
+
+v_smaller1 <- function(x, y, na.rm = FALSE){
+  stopifnot(length(x) == length(y), is.numeric(x) | is.logical(x), 
+            is.numeric(y)| is.logical(x))
+  if (length(x) == 0) return(numeric())
+  simplify2array(
+    Map(function(x, y) smaller(x, y, na.rm = na.rm), x, y)
+  )
+}
+
+v_smaller2 <- function(x, y, na.rm = FALSE) {
+  stopifnot(length(x) == length(y), is.numeric(x) | is.logical(x), 
+            is.numeric(y)| is.logical(x))
+  vapply(seq_along(x), function(i) smaller(x[i], y[i], na.rm = na.rm),
+         numeric(1))
+}
+
+v_smaller1(1:10, c(2,1,4,3,6,5,8,7,10,9))
+#>  [1] 1 1 3 3 5 5 7 7 9 9
+v_smaller2(1:10, c(2,1,4,3,6,5,8,7,10,9))
+#>  [1] 1 1 3 3 5 5 7 7 9 9
+
+v_smaller1(numeric(), numeric())
+#> numeric(0)
+v_smaller2(numeric(), numeric())
+#> numeric(0)
+
+v_smaller1(c(1, NA), c(1, NA), na.rm = FALSE)
+#> [1]  1 NA
+v_smaller2(c(1, NA), c(1, NA), na.rm = FALSE)
+#> [1]  1 NA
+
+v_smaller1(NA,NA)
+#> [1] NA
+v_smaller2(NA,NA)
+#> [1] NA
+
+row_min <- function(x, na.rm = FALSE) {
+  apply(x, 1, r_smaller, na.rm = na.rm)
+}
+col_min <- function(x, na.rm = FALSE) {
+  apply(x, 2, r_smaller, na.rm = na.rm)
+}
+arr_min <- function(x, dim, na.rm = FALSE) {
+  apply(x, dim, r_smaller, na.rm = na.rm)
+}
