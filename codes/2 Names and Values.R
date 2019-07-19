b4e62266a83e16e29c56358eacb7071638ed2e4d
Harry Yang <harry.seunghoon.yang@gmail.com>
2019-07-19 21:40:02 +0900

    chapter2 cleared

--- codes/2 Names and Values.R
+++ codes/2 Names and Values.R
@@ -161,3 +161,118 @@ c
 x <- list(1:10)
 x[[2]] <-  x
 x
+
+#2.4 Object size
+obj_size(letters)
+obj_size(ggplot2::diamonds)
+
+x <- runif(1e6)
+obj_size(x)
+
+y <- list(x, x, x)
+obj_size(y)
+
+obj_size(list(NULL, NULL, NULL))
+
+banana <- "bananas bananas bananas"
+obj_size(banana)
+obj_size(rep(banana, 100))
+
+obj_size(x, y)
+obj_size(1:3)
+
+obj_size(1:1e3)
+obj_size(1:1e6)
+obj_size(1:1e9)
+
+##2.4.1 Exercises
+
+#1
+y <- rep(list(runif(1e4)), 100)
+object.size(y)
+obj_size(y)
+
+?object.size
+
+#2
+funs <- list(mean, sd, var)
+obj_size(funs)
+obj_size(mean)
+obj_size(sd)
+obj_size(var)
+
+#3
+a <- runif(1e6)
+obj_size(a)
+
+b <- list(a, a)
+obj_size(b)
+obj_size(a, b)
+
+b[[1]][[1]] <- 10
+obj_size(b)
+obj_size(a, b)
+
+b[[2]][[1]] <- 10
+obj_size(b)
+obj_size(a, b)
+
+#2.5 Modify-in-place
+
+##2.5.1 Objects with a single binding
+v <- c(1, 2, 3)
+v[[3]] <- 4
+
+#object id is not changed
+
+x <- data.frame(matrix(runif(5 * 1e4), ncol = 5))
+medians <- vapply(x, median, numeric(1))
+
+for (i in seq_along(medians)) {
+  x[[i]] <- x[[i]] - medians[[i]]
+}
+cat(tracemem(x), "\n")
+
+for (i in 1:5) {
+  x[[i]] <- x[[i]] - medians[[i]]
+}
+
+untracemem(x)
+
+y <- as.list(x)
+cat(tracemem(y), "\n")
+
+for (i in 1:5) {
+  y[[i]] <- y[[i]] - medians[[i]]
+}
+
+##2.5.2 Environments
+e1 <- rlang::env(a = 1, b = 2, c = 3)
+e2 <- e1
+
+e1$c <- 4
+e2$c
+
+e <- rlang::env()
+e$self <-  e
+
+##2.5.3 Exercises
+
+#1
+x <- list()
+x[[1]] <- x
+x
+
+#2
+#3
+
+#2.6 Unbinding and the garbage collector
+x <- 1:3
+
+x <- 2:4
+
+rm(x)
+gc()
+
+mem_used()
+
